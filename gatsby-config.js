56696ebc484f3f0900cc68364110abe424e1a42b
Patryk Makowiak <patryk.makowiak@gmail.com>
2020-04-02 18:15:13 +0200

    Modify manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,11 +10,11 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
+        name: process.env.GATSBY_WEBPAGE_NAME,
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#A462FF',
+        theme_color: '#A462FF',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
